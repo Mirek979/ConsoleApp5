5e4039ce2a04147e7c819559c5a1df6cf60aedf1
User <User@MIRV-PC>
2023-08-06 14:34:56 +0200

    Add project files.

---
+++ ConsoleApp5/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Class1
+    { 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="a"></param>
+/// <param name="b"></param>
+/// <returns></returns>
+public double Secti(double a, double b)
+            { return a + b; }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+
+driver.Navigate().GoToUrl("https://wikipedia.org");
+Thread.Sleep(1000);
+
+var element = driver.FindElement(By.Id("searchInput"));
+Thread.Sleep(1000);
+element.SendKeys("java");
+element.SendKeys(Keys.Enter);
+
+
+///
+
+double Secti(double a, double b)
+{
+    return a + b;
+}
+
